5484ababb7c0c9a7bdb4990fb3e6926f6a62af99
CzP <CzP@DESKTOP-BCN0OGB>
2018-11-14 19:26:04 +0100

    Modify filmPanel

--- src/cinema/frontend/windows/FilmPanel.java
+++ src/cinema/frontend/windows/FilmPanel.java
@@ -3,6 +3,7 @@ package cinema.frontend.windows;
 import javax.swing.JPanel;
 import javax.swing.JScrollPane;
 import javax.swing.JTable;
+import javax.swing.event.ListSelectionEvent;
 
 /**
  *
@@ -11,15 +12,24 @@ import javax.swing.JTable;
 public class FilmPanel extends JPanel {
 
     private JTable filmTable;
+    private JPanel filmDetailsPanel;
     
     public FilmPanel() {
         initFilmPanel();
+        initFilmDetailsPanel();
     }
 
     private void initFilmPanel() {
         filmTable = new JTable(4, 5);
-        filmTable.getSelectionModel().addListSelectionListener(filmTable);
         filmTable.getSelectionModel().addListSelectionListener(this::newSelection);
         add(new JScrollPane(filmTable));
+    }
+    
+    private void newSelection(ListSelectionEvent event) {
+        
+    }
+
+    private void initFilmDetailsPanel() {
+        
     }
 }
