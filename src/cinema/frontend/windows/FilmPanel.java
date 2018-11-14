81424a136f4ae13c219f3a0dcc8e317651a6f0b3
CzP <CzP@DESKTOP-BCN0OGB>
2018-11-14 17:25:03 +0100

    initFilmPanel

--- src/cinema/frontend/windows/FilmPanel.java
+++ src/cinema/frontend/windows/FilmPanel.java
@@ -1,6 +1,7 @@
 package cinema.frontend.windows;
 
 import javax.swing.JPanel;
+import javax.swing.JScrollPane;
 import javax.swing.JTable;
 
 /**
@@ -12,6 +13,13 @@ public class FilmPanel extends JPanel {
     private JTable filmTable;
     
     public FilmPanel() {
+        initFilmPanel();
     }
-    
+
+    private void initFilmPanel() {
+        filmTable = new JTable(4, 5);
+        filmTable.getSelectionModel().addListSelectionListener(filmTable);
+        filmTable.getSelectionModel().addListSelectionListener(this::newSelection);
+        add(new JScrollPane(filmTable));
+    }
 }
